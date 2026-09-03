d4c1c78ac821484bb3999b3d67bbba0320c7eada
Mohit Ranjan <mohitranjan2111@gmail.com>
2026-09-03 23:32:05 +0530

    initialize backend project structure with Express, MongoDB connection, and core utility middleware

---
+++ src/middlewares/multer.middleware.js
@@ -0,0 +1,15 @@
+// import express from "express"
+import multer from "multer"
+
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    cb(null, "./public/temp")
+  },
+  filename: function (req, file, cb) {
+    cb(null, `${Date.now()}-${file.originalname}`)
+  }
+})
+
+export const upload = multer({
+    storage
+})
