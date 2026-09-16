ed97ca6f14f402e45a5a8e13473d489b203386ce
Mohit Ranjan <mohitranjan2111@gmail.com>
2026-09-17 00:16:05 +0530

    fix: Configuring Express application and server entry point with database connection

--- src/app.js
+++ src/app.js
@@ -2,19 +2,22 @@ import express from 'express'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 const app = express();
+
+console.log("CORS_ORIGIN:", process.env.CORS_ORIGIN);
 app.use(cors({
     origin: process.env.CORS_ORIGIN,
     credentials: true,
-
 }))
 
-
 app.use(express.json({ limit: "20kb" }))
+
 app.use(express.urlencoded({
     extended: true,
     limit: "20kb"
 }))
+
 app.use(express.static("public"))
+
 app.use(cookieParser())
 
 import { queryRouter } from "./routes/query.route.js"
